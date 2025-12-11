9d8471f40b1fe334b8d92604bc2e823f27d644b4
jangwonik <dhslrl321@gmail.com>
2025-12-12 02:49:35 +0900

    commit

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/dhslrl321.github.io/" // ⚠️ repo 이름으로 변경해야 함
+  base: "" // ⚠️ repo 이름으로 변경해야 함
 })
